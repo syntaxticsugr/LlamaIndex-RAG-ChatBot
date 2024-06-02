ba82fdf96e8a95c6d2ee2564acd66eae165168c2
syntaxticsugr <syntaxticsugr@gmail.com>
2024-06-02 23:40:55 +0530

    02062024E1

--- chatbot.py
+++ chatbot.py
@@ -34,23 +34,33 @@ def app():
                 gr.Markdown("### Step2: (Optional) Change LLM Parameters.")
 
                 with gr.Accordion("Parameters", open=False):
-                    temperature = gr.Slider(minimum = 0.01, maximum = 1.0, value=0.5, step=0.1, label="Temperature", info="Controls randomness in token generation", interactive=True)
-                    maxtokens = gr.Slider(minimum = 128, maximum = 9192, value=4096, step=128, label="Max New Tokens", info="Maximum number of tokens to be generated", interactive=True)
+                    temperature = gr.Slider(minimum = 0.01, maximum = 1.0, value=0.7, step=0.1, label="Temperature", info="Controls randomness in token generation", interactive=True)
+                    maxtokens = gr.Slider(minimum = 128, maximum = 9192, value=256, step=128, label="Max New Tokens", info="Maximum number of tokens to be generated", interactive=True)
                     topk = gr.Slider(minimum = 1, maximum = 10, value=3, step=1, label="top-k", info="Number of tokens to select the next token from", interactive=True)
 
                 init_bot_btn = gr.Button("Initialize ChatBot", variant='primary')
 
                 bot_prg = gr.Markdown("### ChatBot Initialized.")
 
-            gr.Markdown("[//]: # (Works as Spacer)")
-
             with gr.Column(scale=3):
 
                 gr.Markdown("### Step3: Chat with your Documents.")
 
+                chatbot = gr.Chatbot()
+
+                prompt = gr.Textbox(placeholder="Ask Your Question", interactive=True)
+
+                with gr.Row():
+                    submit_btn = gr.Button("Submit")
+                    clear_btn = gr.ClearButton([prompt, chatbot], value="Clear")
+
         vec_db_btn.click(initialize_db, inputs=[documents], outputs=[vec_db, vec_db_prg])
 
         init_bot_btn.click(initialize_bot, inputs=[temperature, maxtokens, topk, vec_db], outputs=[query_engine, bot_prg])
+
+        prompt.submit(getResponse, inputs=[query_engine, prompt, chatbot], outputs=[prompt, chatbot])
+
+        submit_btn.click(getResponse, inputs=[query_engine, prompt, chatbot], outputs=[prompt, chatbot])
 
     app.queue().launch(debug=True)
 
--- chatbot_utils.py
+++ chatbot_utils.py
@@ -1,15 +1,43 @@
-from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
+from llama_index.core import PromptTemplate, SimpleDirectoryReader, VectorStoreIndex
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.huggingface import HuggingFaceLLM
+import torch
+
+
+
+def getResponse(query_engine, prompt, history):
+    response = query_engine.query(prompt).response
+
+    history.append((prompt, response))
+
+    return "", history
 
 
 
 def initialize_bot(temperature, maxtokens, topk, vec_db):
+    system_prompt = """<|SYSTEM|># StableLM Tuned (Alpha version)
+    - StableLM is a helpful and harmless open-source AI language model developed by StabilityAI.
+    - StableLM is excited to be able to help the user, but will refuse to do anything that could be considered harmful to the user.
+    - StableLM is more than just an information source, StableLM is also able to write poetry, short stories, and make jokes.
+    - StableLM will refuse to participate in anything that could harm a human.
+    """
+
+    query_wrapper_prompt = PromptTemplate("<|USER|>{query_str}<|ASSISTANT|>")
+
     llm = HuggingFaceLLM(
-        model_name="NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO",
-        generate_kwargs={"temperature": temperature, "top_k": topk},
-        max_new_tokens=maxtokens
+        context_window=4096,
+        max_new_tokens=maxtokens,
+        generate_kwargs={"temperature": temperature, "do_sample": False},
+        system_prompt=system_prompt,
+        query_wrapper_prompt=query_wrapper_prompt,
+        tokenizer_name="StabilityAI/stablelm-tuned-alpha-3b",
+        model_name="StabilityAI/stablelm-tuned-alpha-3b",
+        device_map="auto",
+        stopping_ids=[50278, 50279, 50277, 1, 0],
+        tokenizer_kwargs={"max_length": 4096},
+        # uncomment this if using CUDA to reduce memory usage
+        model_kwargs={"torch_dtype": torch.float16}
     )
 
     query_engine = vec_db.as_query_engine(llm=llm)
@@ -18,23 +46,17 @@ def initialize_bot(temperature, maxtokens, topk, vec_db):
 
 
 
-def create_db(nodes):
-    embedding = HuggingFaceEmbedding()
-    index = VectorStoreIndex(nodes=nodes, embed_model=embedding)
-    return index
-
-
-
-def load_docs(list_file_path):
-    docs = SimpleDirectoryReader(input_files=list_file_path).load_data()
-    text_splitter = SentenceSplitter(chunk_size=200, chunk_overlap=10)
-    nodes = text_splitter.get_nodes_from_documents(documents=docs)
-    return nodes
-
-
-
 def initialize_db(file_obj_list):
     file_path_list = [x.name for x in file_obj_list if x is not None]
-    nodes = load_docs(file_path_list)
-    vector_db = create_db(nodes)
-    return vector_db, "### Database Created."
+
+    docs = SimpleDirectoryReader(input_files=file_path_list).load_data()
+
+    parser = SentenceSplitter(chunk_size=200, chunk_overlap=10)
+
+    nodes = parser.get_nodes_from_documents(documents=docs)
+
+    embedding = HuggingFaceEmbedding()
+
+    index = VectorStoreIndex(nodes=nodes, embed_model=embedding)
+
+    return index, "### Database Created."
